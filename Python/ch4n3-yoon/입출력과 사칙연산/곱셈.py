6c7d90fe41af4bcc5005e87a6d72a6c4c4693c90
ch4n3 <ch4n3.yoon@gmail.com>
2020-03-09 23:05:23 +0900

    2일차

--- Python/ch4n3-yoon/입출력과 사칙연산/곱셈.py
+++ Python/ch4n3-yoon/입출력과 사칙연산/곱셈.py
@@ -1,13 +1,6 @@
-a = input()
-b = input()
-
-def func(num):
-    sum = 0
-    for i in range(0, 3):
-        sum += int(a[i]) * int('1'+'0'*i) * num
-    return sum
-
-print(func(int(b[0])))
-print(func(int(b[1]) * 10))
-print(func(int(b[2]) * 100)))
+num1 = int(input())
+num2 = int(input())
+for n in map(int, list(str(num2)[::-1])):
+    print(n * num1)
+print(num1 * num2)
 
