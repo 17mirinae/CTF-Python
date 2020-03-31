87d345e9d1fcdc32fc6207b73529c506226e8175
ch4n3-yoon <ch4n3.yoon@gmail.com>
2020-03-31 14:41:31 +0900

    백준 완료

--- Python/SEOKCHAN/재귀/3.별찍기-10.py
+++ Python/SEOKCHAN/재귀/3.별찍기-10.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 # coding: utf-8
+def draw(x, y):
+    while y != 0:
+        if x % 3 == 1 and y % 3 == 1:
+            print(" ", end="")
+            return
+        x = x // 3
+        y = y // 3
+    print("*", end="")
+
+
+def solve(n):
+    for y in range(n):
+        for x in range(n):
+            draw(x, y)
+        print()
+
+
+def main():
+    n = int(input())
+    solve(n)
+
+
+if __name__ == '__main__':
+    main()
 
