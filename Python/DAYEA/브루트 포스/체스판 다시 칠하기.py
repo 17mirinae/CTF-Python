948219ab7a3d7b8d931a4c80fe56127882da3ff2
dayea98 <42088581+dayea98@users.noreply.github.com>
2020-03-31 17:30:11 +0900

    Update 체스판 다시 칠하기.py

--- Python/DAYEA/브루트 포스/체스판 다시 칠하기.py
+++ Python/DAYEA/브루트 포스/체스판 다시 칠하기.py
@@ -1,11 +1,24 @@
 N, M = map(int, input().split())
-L = []
-for _ in range(N):
-    L.append(input())
-
+matrix = []
 cnt = 0
-for i in range(M):
-    if 'BB' in L[i] or 'WW' in L[i]:
-        cnt += 1
+start_B = ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']
+start_W = ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']
+
+for _ in range(N) :
+    matrix.append(list(input()))
+
+if matrix[0][0] == 'W' :
+    for x in range(N) :
+        if x%2 == 0 and matrix[x] != start_W :
+            for y in range(M) :
+                if matrix[x][y] != start_W[y] :
+                    cnt += 1
+        elif x%2 == 1 and matrix[x] != start_B
+else : # 시작이 'B'라면
+    for x in range(N) :
+        if matrix[x] != start_B :
+            for y in range(M) :
+                if matrix[x][y] != start_B[W] :
+                    cnt += 1
 
 print(cnt)
