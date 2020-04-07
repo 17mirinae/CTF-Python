1fe4080bcc2a0a59fb6b5723ab2a94d495f1dc56
KIMDAAAA701 <55473861+KIMDAAAA701@users.noreply.github.com>
2020-04-07 18:02:41 +0900

    no message

---
+++ Python/DAYOUNG/13_백트래킹/7_연산자 끼워넣기.py
@@ -0,0 +1,4 @@
+#https://m.post.naver.com/viewer/postView.nhn?volumeNo=26896712&memberNo=33264526
+
+#문자열로 표현된 파이썬 코드를 실행할 때 사용 : eval()함수
+#https://nan491.tistory.com/entry/Python-3-eval-함수와-exec-함수
---
+++ Python/DAYOUNG/13_백트래킹/8_스타트와 링크.py
@@ -0,0 +1,2 @@
+#뭔소리야,,,
+#https://m.post.naver.com/viewer/postView.nhn?volumeNo=26831431&memberNo=33264526
