eac0061c6513b5d7feabf5d692ecf2a1ecd9fb20
dayea98 <42088581+dayea98@users.noreply.github.com>
2020-04-17 16:31:16 +0900

    Create 포도주 시식.py

---
+++ Python/DAYEA/동적 계획법1/포도주 시식.py
@@ -0,0 +1,14 @@
+N = int(input())
+wine = [int(input()) for _ in range(N)]
+dp = [0] * N
+
+if N == 1:
+    dp[0] = wine[0]
+else:
+    dp[0] = wine[0]
+    dp[1] = wine[0] + wine[1]
+
+for i in range(2, N):
+    dp[i] = max(wine[i] + wine[i - 1] + dp[i - 3], wine[i] + dp[i - 2], dp[i - 1])
+
+print(dp[N])
