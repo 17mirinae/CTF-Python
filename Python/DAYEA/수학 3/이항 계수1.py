ad6c46a0b74d4b0e970684d9199a98f6e5f6b418
dayea98 <42088581+dayea98@users.noreply.github.com>
2020-05-03 18:56:43 +0900

    Create 이항 계수1.py

---
+++ Python/DAYEA/수학 3/이항 계수1.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+
+a = b = c = 1
+for i in range(1, N+1):
+    a *= i
+for j in range(1, K+1):
+    b *= j
+for k in range(1, (N-K)+1):
+    c *= k
+    
+print(int(a/(b*c)))
