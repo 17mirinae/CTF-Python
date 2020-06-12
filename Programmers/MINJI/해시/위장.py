29557d8b4014972b00b407b33854659e93513630
17mirinae <31751083+17mirinae@users.noreply.github.com>
2020-06-12 16:03:16 +0900

    Delete 위장.py

--- Programmers/MINJI/해시/위장.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-from functools import reduce
-from operator import mul
-
-def solution(clothes):
-    return reduce(mul, [cnt + 1 for cnt in Counter([cat for _, cat in clothes]).values()]) - 1
